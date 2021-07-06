f09d5035066eb554c8166a0b08665c625f6ccf06
Ericaasca <54380638+Ericaasca@users.noreply.github.com>
2021-07-06 16:32:42 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+const port = process.env.PORT || 3000;
+const Blowfish = require("javascript-blowfish").Blowfish;
+var bf = new Blowfish("chave secreta");
+
+app.use(express.static(__dirname + '/scripts/'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    msg = bf.encrypt(msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
